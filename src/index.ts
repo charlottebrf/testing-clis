2221a50667aa5102d7c5edb066038ddd9a80c3bc
Charlotte Fereday <cbrfereday@gmail.com>
2021-05-13 22:56:39 +0100

    Includes Inquirer for prompts
    Useful set of examples for usage here: https://github.com/SBoudrias/Inquirer.js/tree/master/packages/inquirer/examples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {Command, flags} from '@oclif/command'
+import * as inquirer from 'inquirer'
 const fs = require('fs');
 const path = require('path')
 
@@ -10,12 +11,12 @@ class Mynewcli extends Command {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
     path: flags.string({char: 'p', description: 'full path of current file to mirror test file against'}),
+    type: flags.string({options: ['e2e', 'off', 'plt', 'svc'], description: 'choose the type of test you would like'})
   }
   static args = [{name: 'fileType'}]
 
-
   async run() {
-    const {args, flags} = this.parse(Mynewcli)
+    const {flags} = this.parse(Mynewcli)
     if (!flags.path) {
       throw new Error('you must provide a file path to use this flag!')
     };
@@ -25,13 +26,83 @@ class Mynewcli extends Command {
     }
 
     const baseName = path.basename(flags.path).split('.')[0];
-    
-    // check if args.fileType is a valid file type
-    const testFileType = args.fileType ?? 'svc';
-    const testFileName = `${baseName}.spec.${testFileType}.ts`;
-    fs.writeFile(path.join(__dirname, testFileName), 'Hey some awesome test file content', function (err: Error) {
-      if (err) throw new Error('Something went wrong, we could not create the test file')
-    });
+    let type = flags.type;
+
+
+    if (!type) {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'What kind of test file do you need?',
+        type: 'list',
+        choices: [{name: 'e2e'}, {name: 'off'}, {name: 'plt'}, {name: 'svc'}],
+      }])
+      type = responses.type
+    }
+
+    if (type === 'svc') {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'Please select to confirm that for your test you will need',
+        type: 'checkbox',
+        choices: [
+          new inquirer.Separator('**Test characteristics**'),
+          {name: 'Needs no network access'}, {name: 'Can access service owned resources'}, {name: 'Optimized for fast feedback'}],
+      }])
+      if (responses.type.length < 3) {
+         throw new Error('You must choose all service test characteristics: as you did not consider starting again & choosing another more suitable test');
+      }
+    }
+
+
+    if (type === 'off') {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'Please select to confirm that for your test you will need',
+        type: 'checkbox',
+        choices: [
+          new inquirer.Separator('**Test characteristics**'),
+          {name: 'Tests small units of code'}, {name: 'No database or network availability'}, {name: 'Optimized for very fast feedback'}],
+      }])
+      if (responses.type.length < 3) {
+         throw new Error('You must choose all unit test characteristics: as you did not, consider starting again & choosing another more suitable test');
+      }
+    }
+
+
+
+    if (type === 'plt') {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'Please select to confirm that for your test you will need',
+        type: 'checkbox',
+        choices: [
+          new inquirer.Separator('**Test characteristics**'),
+          {name: 'Tests the services integrations against other services'}, {name: 'Vulnerable to flakiness'}, {name: 'Slower to execute'}],
+      }])
+      if (responses.type.length < 3) {
+         throw new Error('You must choose all service test characteristics: as you did not, consider starting again & choosing another more suitable test');
+      }
+    }
+
+
+    if (type === 'e2e') {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'Please select to confirm that for your test you will need',
+        type: 'checkbox',
+        choices: [
+          new inquirer.Separator('**Test characteristics**'),
+          {name: 'Test against a 3rd party'}, {name: 'Test entire application flows'}, {name: 'Slow to run, expensive'}],
+      }])
+      if (responses.type.length < 3) {
+         throw new Error('You must choose all service test characteristics: as you did not, consider starting again & choosing another more suitable test');
+      }
+    }
+
+    const testFileName = `${baseName}.spec.${type}.ts`;
+      fs.writeFile(path.join(__dirname, testFileName), 'Hey some awesome test file content', function (err: Error) {
+        if (err) throw new Error('Something went wrong, we could not create the test file')
+      });
   }
 }
 
